d22c1103ac831729e4d34a8cef6b1af24864194c
cppfed <a.fedorov.work@gmail.com>
2021-06-06 19:26:21 +0300

    Add ticker

--- internal/saver/saver.go
+++ internal/saver/saver.go
@@ -1,13 +1,15 @@
 package saver
 
 import (
+	"fmt"
 	"github.com/ozoncp/ocp-solution-api/internal/flusher"
 	"github.com/ozoncp/ocp-solution-api/internal/models"
+	"time"
 )
 
 type Saver interface {
 	Save(solution models.Solution)
-	Close()
+	Close() error
 }
 
 type saver struct {
@@ -15,7 +17,7 @@ type saver struct {
 	slice               []models.Solution
 	forgetAllOnOverflow bool
 	f                   flusher.Flusher
-	// TODO: add timer
+	doneCh              chan bool
 }
 
 func (s *saver) Save(solution models.Solution) {
@@ -23,24 +25,46 @@ func (s *saver) Save(solution models.Solution) {
 		if s.forgetAllOnOverflow {
 			s.slice = make([]models.Solution, 0, s.capacity)
 		} else {
-			halfCapacity := s.capacity / 2
+			halfCapacity := int(s.capacity / 2)
 			s.slice = s.slice[halfCapacity:]
 		}
 	}
 	s.slice = append(s.slice, solution)
 }
 
-func (s *saver) Close() {
-	// TODO: flush
+func (s *saver) Close() error {
+	var err error
+	if s.slice, err = s.f.Flush(s.slice); err != nil {
+		err = fmt.Errorf("lost %v solutions: %w", len(s.slice), err)
+	}
+	close(s.doneCh)
+	return err
 }
 
-// TODO: flush on timer timeout
-
 func New(capacity uint, flusher flusher.Flusher, forgetAllOnOverflow bool) Saver {
-	return &saver{
+	s := &saver{
 		capacity:            capacity,
 		slice:               make([]models.Solution, 0, capacity),
 		forgetAllOnOverflow: forgetAllOnOverflow,
 		f:                   flusher,
+		doneCh:              make(chan bool),
 	}
+
+	const tickerPeriod = 5 * time.Second
+
+	go func() {
+		ticker := time.NewTicker(tickerPeriod)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ticker.C:
+				s.slice, _ = s.f.Flush(s.slice)
+			case <-s.doneCh:
+				return
+			}
+		}
+	}()
+
+	return s
 }
