45162e7d3b84046423172aa0eaf4ce19ec3ae433
fedorov-cpp <a.fedorov.work@gmail.com>
2021-06-07 19:02:38 +0300

    Protect slice with mutex

--- internal/saver/saver.go
+++ internal/saver/saver.go
@@ -5,6 +5,7 @@ import (
 	"github.com/ozoncp/ocp-solution-api/internal/flusher"
 	"github.com/ozoncp/ocp-solution-api/internal/models"
 	"github.com/ozoncp/ocp-solution-api/internal/utils"
+	"sync"
 	"time"
 )
 
@@ -15,6 +16,7 @@ type Saver interface {
 
 type saver struct {
 	capacity            uint
+	mtx                 sync.Mutex
 	slice               []models.Solution
 	forgetAllOnOverflow bool
 	f                   flusher.Flusher
@@ -22,6 +24,9 @@ type saver struct {
 }
 
 func (s *saver) Save(solution models.Solution) {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+
 	if len(s.slice) == cap(s.slice) {
 		if s.forgetAllOnOverflow {
 			s.slice = make([]models.Solution, 0, s.capacity)
@@ -34,6 +39,9 @@ func (s *saver) Save(solution models.Solution) {
 }
 
 func (s *saver) Close() error {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
+
 	var err error
 	if s.slice, err = s.f.Flush(s.slice); err != nil {
 		err = fmt.Errorf("lost %v solutions: %w", len(s.slice), err)
@@ -53,6 +61,7 @@ func New(capacity uint, flusher flusher.Flusher, forgetAllOnOverflow bool) (Save
 
 	s := &saver{
 		capacity:            capacity,
+		mtx:                 sync.Mutex{},
 		slice:               make([]models.Solution, 0, capacity),
 		forgetAllOnOverflow: forgetAllOnOverflow,
 		f:                   flusher,
